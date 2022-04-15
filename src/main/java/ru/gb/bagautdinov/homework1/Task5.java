2e5c76eecb5cc35700a9c2225e795783936a8689
Rustam <r.bagautdinov275@gmail.com>
2022-04-15 17:31:33 +0500

    The fifth task is completed

---
+++ src/main/java/ru/gb/bagautdinov/homework1/Task5.java
@@ -0,0 +1,16 @@
+package ru.gb.bagautdinov.homework1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+    public static void compareNumbers() {
+        int a = 2;
+        int b = 3;
+        if (a >= b){
+            System.out.println("a >= b");
+        }else {
+            System.out.println("a < b");
+        }
+    }
+}
