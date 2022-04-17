c1e53fa87ead88860c8bb137c0234239425c0b4b
Rustam <r.bagautdinov275@gmail.com>
2022-04-17 11:01:33 +0500

    Added a fourth task

---
+++ src/main/java/ru/gb/bagautdinov/homework2/Task4.java
@@ -0,0 +1,13 @@
+package ru.gb.bagautdinov.homework2;
+
+public class Task4 {
+    public static void main(String[] args) {
+        ineOutput();
+    }
+    public static void ineOutput(){
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Hello world");
+        }
+    }
+
+}
