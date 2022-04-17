acdc86c227af4d30abdb648d5cc330e8c0f386b9
Rustam <r.bagautdinov275@gmail.com>
2022-04-17 10:59:19 +0500

    Added a third task

---
+++ src/main/java/ru/gb/bagautdinov/homework2/Task3.java
@@ -0,0 +1,15 @@
+package ru.gb.bagautdinov.homework2;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println(determiningTheValue(-36));
+    }
+    public static boolean  determiningTheValue(int a){
+
+        if (a < 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
