6cf1346023a64f954650f1c388b25ce403686330
Rustam <r.bagautdinov275@gmail.com>
2022-04-17 10:51:55 +0500

    The second task has been added

---
+++ src/main/java/ru/gb/bagautdinov/homework2/Task1.java
@@ -0,0 +1,14 @@
+package ru.gb.bagautdinov.homework2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(checking(5, 8));
+    }
+    public static boolean checking(int a, int b) {
+        if ((a + b) >= 10 && (a + b) <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
