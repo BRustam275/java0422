ab18a14fb68556dd162cc872e0f0c0c69f5acba3
Rustam <r.bagautdinov275@gmail.com>
2022-04-20 17:00:23 +0500

    Task 2

--- src/main/java/ru/gb/bagautdinov/homework3/ReplacingAnArray.java
+++ src/main/java/ru/gb/bagautdinov/homework3/ReplacingAnArray.java
@@ -1,5 +1,6 @@
 package ru.gb.bagautdinov.homework3;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class ReplacingAnArray {
@@ -10,28 +11,30 @@ public class ReplacingAnArray {
     }
     public static void array() {
         int[] a = {0, 0, 1, 0, 1, 1, 1, 0, 1, 1};
+        System.out.println(Arrays.toString(a));
         for (int i = 0; i < a.length; i++) {
             if (a[i] == 0) {
-                System.out.print("1, ");
+               a[i] = 1;
             } else {
-                System.out.print("0, ");
+               a[i] = 0;
             }
         }
+        System.out.println(Arrays.toString(a));
     }
     public static void arrayRandom() {
         int[] a = new int[10];
         for (int i = 0; i < a.length; i++) {
             a[i] = new Random().nextInt(2);
-            System.out.print (a[i] + ", ");
-        }
-        System.out.println();
+            }
+        System.out.println(Arrays.toString(a));
+
         for (int i = 0; i < a.length; i++) {
             if (a[i] == 0) {
-                System.out.print("1, ");
+                a[i] = 1;
             } else {
-                System.out.print("0, ");
+               a[i] = 0;
             }
-
         }
+        System.out.println(Arrays.toString(a));
     }
 }
